e3b6e0cc03330d1a777491029e3de8193e01223b
jenfonro <799170122@qq.com>
2025-11-07 11:42:24 +0800

    refactor(worker): implement immediate retry in same worker (#1)
    * refactor(worker): implement immediate retry in same worker
    
    * return
    
    * refactor(worker): streamline retry logic in task execution
    
    - Moved retry state handling to a more logical position within the execution loop.
    - Improved readability by reducing unnecessary code duplication.
    - Ensured that the task state is checked correctly before returning from the loop.
    
    ---------
    
    Co-authored-by: Andy Hsu <i@nn.ci>

--- worker.go
+++ worker.go
@@ -16,12 +16,6 @@ type Worker[T Task] struct {
 
 // Execute executes the task
 func (w Worker[T]) Execute(task T) {
-	if isRetry(task) {
-		task.SetState(StateBeforeRetry)
-		if hook, ok := Task(task).(OnBeforeRetry); ok {
-			hook.OnBeforeRetry()
-		}
-	}
 	onError := func(err error) {
 		task.SetErr(err)
 		if errors.Is(err, context.Canceled) {
@@ -37,23 +31,41 @@ func (w Worker[T]) Execute(task T) {
 			task.SetState(StateFailed)
 		}
 	}
-	defer func() {
-		if err := recover(); err != nil {
-			log.Printf("error [%s] while run task [%s],stack trace:\n%s", err, task.GetID(), getCurrentGoroutineStack())
-			onError(NewErr(fmt.Sprintf("panic: %v", err)))
+	// Retry immediately in the same worker until success or max retry exhausted
+	for {
+		func() {
+			defer func() {
+				if err := recover(); err != nil {
+					log.Printf("error [%s] while run task [%s],stack trace:\n%s", err, task.GetID(), getCurrentGoroutineStack())
+					onError(NewErr(fmt.Sprintf("panic: %v", err)))
+				}
+			}()
+
+			if isRetry(task) {
+				task.SetState(StateBeforeRetry)
+				if hook, ok := Task(task).(OnBeforeRetry); ok {
+					hook.OnBeforeRetry()
+				}
+			}
+
+			task.SetState(StateRunning)
+			err := task.Run()
+			if err != nil {
+				onError(err)
+				return
+			}
+
+			task.SetState(StateSucceeded)
+			if onSucceeded, ok := Task(task).(OnSucceeded); ok {
+				onSucceeded.OnSucceeded()
+			}
+			task.SetErr(nil)
+		}()
+
+		if task.GetState() != StateWaitingRetry {
+			return
 		}
-	}()
-	task.SetState(StateRunning)
-	err := task.Run()
-	if err != nil {
-		onError(err)
-		return
 	}
-	task.SetState(StateSucceeded)
-	if onSucceeded, ok := Task(task).(OnSucceeded); ok {
-		onSucceeded.OnSucceeded()
-	}
-	task.SetErr(nil)
 }
 
 // WorkerPool is the pool of workers
